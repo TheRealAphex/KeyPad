d19f79474e8975c48f084beb5bed1a8a59aad2e6
TheRealAphex <thearstar@gmail.com>
2014-01-02 17:16:57 -0600

    KeyPad made in xcode

--- KeyPad/ViewController.h
+++ KeyPad/ViewController.h
@@ -10,4 +10,17 @@
 
 @interface ViewController : UIViewController
 
+@property(nonatomic,retain) IBOutlet UILabel *keyPadText;
+
+
+-(IBAction)B1:(id)sender;
+-(IBAction)B2:(id)sender;
+-(IBAction)B3:(id)sender;
+-(IBAction)B4:(id)sender;
+-(IBAction)B5:(id)sender;
+-(IBAction)B6:(id)sender;
+-(IBAction)B7:(id)sender;
+-(IBAction)B8:(id)sender;
+-(IBAction)B9:(id)sender;
+-(IBAction)BckSpc:(id)sender;
 @end
--- KeyPad/ViewController.m
+++ KeyPad/ViewController.m
@@ -14,6 +14,77 @@
 
 @implementation ViewController
 
+@synthesize keyPadText;
+
+
+
+-(IBAction)B1:(id)sender{
+    
+    keyPadText.text = [keyPadText.text stringByAppendingString:@"1"];
+}
+
+
+-(IBAction)B2:(id)sender{
+    
+    keyPadText.text = [keyPadText.text stringByAppendingString:@"2"];
+}
+
+
+-(IBAction)B3:(id)sender{
+    
+    keyPadText.text = [keyPadText.text stringByAppendingString:@"3"];
+}
+
+
+-(IBAction)B4:(id)sender{
+    
+    keyPadText.text = [keyPadText.text stringByAppendingString:@"4"];
+}
+
+
+-(IBAction)B5:(id)sender{
+    
+    keyPadText.text = [keyPadText.text stringByAppendingString:@"5"];
+}
+
+
+-(IBAction)B6:(id)sender{
+    
+    keyPadText.text = [keyPadText.text stringByAppendingString:@"6"];
+}
+
+
+-(IBAction)B7:(id)sender{
+    
+    keyPadText.text = [keyPadText.text stringByAppendingString:@"7"];
+}
+
+
+-(IBAction)B8:(id)sender{
+    
+    keyPadText.text = [keyPadText.text stringByAppendingString:@"8"];
+}
+
+
+-(IBAction)B9:(id)sender{
+    
+    keyPadText.text = [keyPadText.text stringByAppendingString:@"9"];
+}
+
+-(IBAction)BckSpc:(id)sender{
+    NSString *backspace = keyPadText.text;
+	int lengthofstring = backspace.length;
+    
+    if (lengthofstring == 0){
+	backspace = [backspace substringToIndex:lengthofstring - 0];
+	keyPadText.text = backspace;
+    } else {
+        backspace = [backspace substringToIndex:lengthofstring - 1];
+        keyPadText.text = backspace;
+    }
+}
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
